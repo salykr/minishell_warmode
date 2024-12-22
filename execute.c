4305b7ede953cf482a49da0040afa5e706a29aed
sally <sallykreik@gmail.com>
2024-12-22 11:32:21 +0200

    fixed pipes

--- execute.c
+++ execute.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skreik <skreik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 12:56:23 by skreik            #+#    #+#             */
-/*   Updated: 2024/12/11 13:00:37 by skreik           ###   ########.fr       */
+/*   Updated: 2024/12/22 11:03:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_shell.h"
 
-void	execute_command(t_parser *parser, t_fd f, t_env *env)
+void	execute_command(t_parser *parser, t_fd f, t_env *env, int fd[2])
 {
 	char	*cmd_path;
 	char	**args;
 	int		heredoc_fd;
 	pid_t	pid;
+	(void)fd;
 
 	args = initialize_execution(&heredoc_fd, parser, env, &cmd_path);
 	printf("args\n");
@@ -41,17 +42,43 @@ void	execute_command(t_parser *parser, t_fd f, t_env *env)
 	}
 }
 
-void	execute_builtin_command(t_parser *parser, t_fd f, t_env *env)
+int save_original_fds(int *original_stdin, int *original_stdout) {
+    *original_stdin = dup(STDIN_FILENO);
+    *original_stdout = dup(STDOUT_FILENO);
+    if (*original_stdin == -1 || *original_stdout == -1) {
+        perror("Error saving original file descriptors");
+        return -1;
+    }
+    return 0;
+}
+void restore_original_fds(int original_stdin, int original_stdout) {
+    if (dup2(original_stdin, STDIN_FILENO) == -1) {
+        perror("Error restoring STDIN");
+    }
+    if (dup2(original_stdout, STDOUT_FILENO) == -1) {
+        perror("Error restoring STDOUT");
+    }
+    close(original_stdin);
+    close(original_stdout);
+}
+
+void	execute_builtin_command(t_parser *parser, t_fd f, t_env *env, int fd[2])
 {
 	pid_t	pid;
 	int heredoc_fd;
-		printf("parent pid %d\n",getpid());
-
-	initialize_heredoc(&heredoc_fd, parser);
-	//&&(strcmp(parser->command, "exit") == 0 || strcmp(parser->command, "cd") == 0)
-	if (parser -> next == NULL)
-		buitlin(parser, env);//ll exit
+	printf("parent pid %d\n",getpid());
+	int original_stdin, original_stdout;
+	(void)fd;
 	
+	initialize_heredoc(&heredoc_fd, parser);
+ 	if (parser->next == NULL) 
+	{
+        if (save_original_fds(&original_stdin, &original_stdout) == -1)
+            return; // Handle error
+        manage_input_output(-1, &f);
+			buitlin(parser, env);
+        restore_original_fds(original_stdin, original_stdout);
+    } 
 	else
 	{
 		pid = fork();
@@ -59,7 +86,7 @@ void	execute_builtin_command(t_parser *parser, t_fd f, t_env *env)
 			exit(EXIT_FAILURE);
 		if (pid == 0) // Child process
 		{
-			manage_input_output(-1, &f);
+			manage_input_output(-1,&f);
 			buitlin(parser, env);
 			exit(global_var);
 		}
@@ -112,6 +139,9 @@ void	cmds_exec(t_parser *parser, t_env *env)
 	t_fd f;
 	int fd[2];
 	f.fd_1 = STDIN_FILENO;
+	int status;
+	fd[0] = -1;
+	fd[1] = -1;
 
 	while (parser)
 	{
@@ -120,9 +150,9 @@ void	cmds_exec(t_parser *parser, t_env *env)
 		input_redirection(parser, &f);
 		output_redirection(parser, &f, fd);
 		if (is_builtin(parser))
-			execute_builtin_command(parser, f, env);
+			execute_builtin_command(parser, f, env, fd);
 		else
-			execute_command(parser, f, env);
+			execute_command(parser, f, env, fd);
 		if (parser->next) // to handle input of cmd
 		{
 			close(fd[1]);
@@ -132,4 +162,71 @@ void	cmds_exec(t_parser *parser, t_env *env)
 	}
 	if (f.fd_1 != STDIN_FILENO)
 		close(f.fd_1);
+	while (wait(&status) > 0)
+	{
+		if (WIFEXITED(status))
+			global_var = WEXITSTATUS(status);
+	}
+
 }
+// void cmds_exec(t_parser *parser, t_env *env)
+// {
+//     t_fd f;
+//     int fd[2];
+//     int status;
+//     pid_t pid;
+
+//     f.fd_1 = STDIN_FILENO;
+
+//     while (parser)
+//     {
+//         if (parser->next && pipe(fd) == -1)
+//         {
+//             perror("Pipe failed");
+//             exit(EXIT_FAILURE);
+//         }
+
+//         pid = fork();
+//         if (pid == -1)
+//         {
+//             perror("Fork failed");
+//             exit(EXIT_FAILURE);
+//         }
+
+//         if (pid == 0) // Child process
+//         {
+//             if (parser->next)
+//             {
+//                 close(fd[0]);            // Close read end for child
+//                 dup2(fd[1], STDOUT_FILENO); // Redirect output to pipe
+//                 close(fd[1]);
+//             }
+//             if (f.fd_1 != STDIN_FILENO)
+//             {
+//                 dup2(f.fd_1, STDIN_FILENO); // Redirect input
+//                 close(f.fd_1);
+//             }
+
+//             if (is_builtin(parser))
+//                 buitlin(parser, env);
+//             else
+//                 execute_command(parser, f, env);
+//             exit(EXIT_SUCCESS);
+//         }
+//         else // Parent process
+//         {
+//             if (f.fd_1 != STDIN_FILENO)
+//                 close(f.fd_1); // Close input fd for parent
+
+//             if (parser->next)
+//             {
+//                 close(fd[1]);    // Close write end in parent
+//                 f.fd_1 = fd[0];  // Set read end for next command
+//             }
+//         }
+//         parser = parser->next;
+//     }
+
+//     while (wait(&status) > 0); // Wait for all child processes to terminate
+// }
+
