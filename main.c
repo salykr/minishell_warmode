c8591c83fb4b2e4d056b2458ef08053060d20e56
Issa Alayan <isalayan@c2r1s9.42beirut.com>
2025-01-05 17:06:09 +0200

    fixed redirection

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skreik <skreik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: isalayan <isalayan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:10:16 by skreik            #+#    #+#             */
-/*   Updated: 2025/01/05 13:38:56 by skreik           ###   ########.fr       */
+/*   Updated: 2025/01/05 16:22:59 by isalayan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int	initiate_input(char *line, t_tokenlist **token_list,
 	return (1);
 }
 
-int	main(int argc, char **argv, char **envp)
+int	main1(int argc, char **argv, char **envp)
 {
 	char		*line;
 	t_env		*my_env;
@@ -75,66 +75,66 @@ int	main(int argc, char **argv, char **envp)
 	return (ft_free_env(&my_env), EXIT_SUCCESS);
 }
 
-// int     main1(int argc, char **argv, char **envp)
-// {
-// 	(void)argc;
-// 	(void)argv;
-// 	char *line = NULL;
-// 	t_env *my_env;
-// 	t_tokenlist *token_list;
-// 	t_parser *parser;
-// 	int value ;
-// 	if(envp == NULL || *envp == NULL)
-// 		my_env = manage_env_i();
-// 	else
-// 		my_env = init_env(envp);
-// 	restore_signals();
-// 	while (1)
-// 	{
-// 		line = readline("minishell>");
-// 		if (!line)
-// 			break ;
-// 		if (ft_strlen(line) == 0)
-// 		{
-// 			free(line);
-// 			continue;
-// 		}
-// 		if (*line)
-// 			add_history(line);
-// 		token_list = tokenize_input(line);
-// 		if (!token_list)
-// 		{
-// 			printf("Error: Failed to tokenize input.\n");
-// 			free(line);
-// 			continue ;
-// 		}
-// 		// Print tokens (for debugging)
-// 		printf("\nTokens:______________________\n");
-// 		print_token_list(token_list);
-// 		// Parse tokens
-// 		parser = create_parser(); // Initialize parser
-// 		if (!parser)
-// 		{
-// 			printf("Error: Failed to create parser.\n");
-// 			free(line);
-// 			free_token_list(token_list); // Free token list
-// 			continue ;
-// 		}
-// 		value = parse_tokens(&parser, token_list, *my_env);
-// 		if (!parser || !parser->command || value == -1)
-// 		{
-// 			free(line);
-// 			free_token_list(token_list);
-// 			free_parser(parser);
-// 			continue ;
-// 		}
-// 		printf("\nParsed commands:\n");
-// 		print_parser(parser);
-// 		cmds_exec(parser, my_env);
-// 		free_token_list(token_list);
-// 		free(line);
-// 		free_parser(parser);
-// 	}
-// 	ft_free_env(&my_env);
-// 	return (EXIT_SUCCESS);
-// }
+int     main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	char *line = NULL;
+	t_env *my_env;
+	t_tokenlist *token_list;
+	t_parser *parser;
+	int value ;
+	if(envp == NULL || *envp == NULL)
+		my_env = manage_env_i();
+	else
+		my_env = init_env(envp);
+	restore_signals();
+	while (1)
+	{
+		line = readline("minishell>");
+		if (!line)
+			break ;
+		if (ft_strlen(line) == 0)
+		{
+			free(line);
+			continue;
+		}
+		if (*line)
+			add_history(line);
+		token_list = tokenize_input(line);
+		if (!token_list)
+		{
+			printf("Error: Failed to tokenize input.\n");
+			free(line);
+			continue ;
+		}
+		// Print tokens (for debugging)
+		printf("\nTokens:______________________\n");
+		print_token_list(token_list);
+		// Parse tokens
+		parser = create_parser(); // Initialize parser
+		if (!parser)
+		{
+			printf("Error: Failed to create parser.\n");
+			free(line);
+			free_token_list(token_list); // Free token list
+			continue ;
+		}
+		value = parse_tokens(&parser, token_list, *my_env);
+		if (!parser || !parser->command || value == -1)
+		{
+			free(line);
+			free_token_list(token_list);
+			free_parser(parser);
+			continue ;
+		}
+		printf("\nParsed commands:\n");
+		print_parser(parser);
+		cmds_exec(parser, my_env);
+		free_token_list(token_list);
+		free(line);
+		free_parser(parser);
+	}
+	ft_free_env(&my_env);
+	return (EXIT_SUCCESS);
+}
